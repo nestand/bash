3ff97aa0f6a87d8e030b5fa97c9320158d62d553
Andrey Nesterov <andrey.nesterov@laposte.net>
2024-09-16 14:10:36 +0200

    ping remote host sh. V with cheking the  IP

--- advanced_scripts/ping.sh
+++ advanced_scripts/ping.sh
@@ -2,3 +2,18 @@
 
 #the script to ping the remote host
 
+# -c -> count (how many times we ping the ip)
+# -w -> timeout (in sec) 
+
+#ping ip that works
+#ping -c1 142.250.178.142
+
+#ping ip that doesn't work 
+ping -c1 168.192.1.256
+
+if [ $? -eq 0 ]
+then
+echo "OK"
+else 
+echo "not OK"
+fi
